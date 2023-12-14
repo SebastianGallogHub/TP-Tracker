7ec6bb32375bbf7527ebf2d16e71162a45d491c4
Sebastián Gallo <1sebastiangallo@gmail.com>
2023-12-14 09:52:30 -0300

    Agregado de fitros. Corrección de offset. Aún no funciona.

--- application/src/measurePosition.c
+++ application/src/measurePosition.c
@@ -17,13 +17,14 @@
 #include "kalman.h"
 
 /*==================[macros and typedef]=====================================*/
-#define DATA_RATE  		1/800 // 800Hz
+#define DATA_RATE  		1/400 // 400Hz
 #define MAX_VALUES  	2
 #define MAX_AXIS		3
-#define MAX_SAMPLES 	64
-#define MAX_SAMPLES_2P	6
-#define LOW_LEVEL_NOISE	3
-#define T				DATA_RATE * MAX_SAMPLES
+#define SAMPLES 		64
+#define SAMPLES_2P		6
+#define LOW_LEVEL_NOISE	12
+#define END_OF_MOVEMENT 2
+#define T				DATA_RATE * SAMPLES
 
 /*==================[internal functions declaration]=========================*/
 static void measurePositionTask(void *pvParameters);
@@ -33,8 +34,9 @@ static void measurePositionTask(void *pvParameters);
 
 static position3D_t position;
 static uint16_t contAcc = 0;
-static uint8_t getOffset = 0;
-static int16_t OFF[MAX_AXIS];
+static uint8_t getOffset = 1;
+static int16_t OFF[MAX_AXIS],
+			   END_OF_MOVEMENT_CT[MAX_AXIS];
 static int16_t A[MAX_VALUES][MAX_AXIS];
 static float_t V[MAX_VALUES][MAX_AXIS],
  	 	 	   P[MAX_VALUES][MAX_AXIS];
@@ -53,14 +55,6 @@ static void measurePositionTask(void *pvParameters)
 	position.Y = 0;
 	position.Z = 0;
 
-	for (i = 0; i < MAX_VALUES; i++) {
-		for (j = 0; j < MAX_AXIS; j++) {
-			A[i][j] = 0;
-			V[i][j] = 0;
-			P[i][j] = 0;
-		}
-	}
-
 	//Delay para esperar que los recursos se inicialicen en otra tarea
 	vTaskDelay(500 / portTICK_PERIOD_MS);
 	for (;;)
@@ -71,29 +65,34 @@ static void measurePositionTask(void *pvParameters)
 			accCAD = mma8451_getAccIntCount();
 
 			if (getOffset) {
+				for (i = 0; i < MAX_VALUES; i++) {
+					for (j = 0; j < MAX_AXIS; j++) {
+						A[i][j] = 0;
+						V[i][j] = 0;
+						P[i][j] = 0;
+						END_OF_MOVEMENT_CT[j] = 0;
+					}
+				}
 				//Primera calibración
-				OFF[0] += accCAD.accX;
-				OFF[1] += accCAD.accY;
-				OFF[2] += 4096 - accCAD.accZ; //Quitar el campo gravitatorio (1G)
+				OFF[0] = accCAD.accX;
+				OFF[1] = accCAD.accY;
+				OFF[2] = 4096 - accCAD.accZ; //Quitar el campo gravitatorio (1G)
+				getOffset = 0;
 			}
 
 			A[1][0] += accCAD.accX;
 			A[1][1] += accCAD.accY;
 			A[1][2] += 4096 - accCAD.accZ; //Quitar el campo gravitatorio (1G)
 
-			if (contAcc == 3) {
-				A[1][0] += accCAD.accX;
-			}
-
-			if (contAcc >=64) {
+			if (contAcc >= SAMPLES) {
 				contAcc = 0;
 
 				for (i = 0; i < MAX_AXIS; i++) {
 					//Promediado
-					A[1][i] = A[1][i]>>MAX_SAMPLES_2P; //div 64
+					A[1][i] = A[1][i] / SAMPLES; //div 64
 
 					//Se quita el offset
-					A[1][i] = OFF[i];
+					A[1][i] -= OFF[i];
 
 					//Filtrado ruido HW
 					if ((A[1][i] <= LOW_LEVEL_NOISE) && (A[1][i] >= -LOW_LEVEL_NOISE)) {
@@ -114,11 +113,24 @@ static void measurePositionTask(void *pvParameters)
 					A[0][i] = A[1][i];
 					V[0][i] = V[1][i];
 					P[0][i] = P[1][i];
+
+					A[1][i] = 0;
+					V[1][i] = 0;
+					P[1][i] = 0;
+
+					if (A[0][i] == 0) {
+						END_OF_MOVEMENT_CT[i]++;
+					}
+
+					if (END_OF_MOVEMENT_CT[i] >= END_OF_MOVEMENT) {
+						V[0][i] = 0;
+						V[1][i] = 0;
+					}
 				}
 
-				position.X = P[1][0];
-				position.Y = P[1][1];
-				position.Z = P[1][2];
+				position.X = P[0][0];
+				position.Y = P[0][1];
+				position.Z = P[0][2];
 
 				reportPosition_addNewPosition(&position);
 			}
